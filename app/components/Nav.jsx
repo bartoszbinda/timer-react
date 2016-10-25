9227e5b5f81cd29fc375b8de91b09012f8118fde
Bartosz <batinex@gmail.com>
2016-10-25 20:03:09 +0200

    Styled navbar:

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,23 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
-var Nav = (props) => {
+var Nav = () => {
   return(
     <div className='top-bar'>
       <div className='top-bar-left'>
         <ul className='menu'>
-          <li className='menu-text'>Timer&Countdown Application</li>
+          <li className='menu-text'>React Timer</li>
           <li>
             <IndexLink to="/" activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
           </li>
           <li>
             <Link to='/countdown' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className='top-bar-right'>
+        <ul className='menu'>
+          <li className='menu-text'>
+            Created by <a href="http://www.github.com/bartoszbinda" target='_blank' ref='nnoopener norefferer'>Bartosz Binda</a>
           </li>
         </ul>
       </div>

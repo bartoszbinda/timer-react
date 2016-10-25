b903c15cd85aaab0aba4bb2538161209cfeffee0
Bartosz <batinex@gmail.com>
2016-10-25 22:46:25 +0200

    somethings wrong

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var webpack = require('webpack');
 
 module.exports = {
-  entry: ['script!jquery/dist/jquery.min.js',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
   'script!foundation-sites/dist/foundation.min.js',
   './app/app.jsx'],
   externals: {

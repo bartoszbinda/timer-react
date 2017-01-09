878904169b82ec6372fc198d61ae41a9eaa802ac
Bartosz <batinex@gmail.com>
2017-01-09 23:10:00 +0100

    created controls component and test for it

---
+++ app/tests/Controls.test.jsx
@@ -0,0 +1,12 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jquery');
+var Controls = require('Controls');
+
+describe('Controls', () => {
+  it('should exists', () => {
+    expect(Controls).toExist();
+  });
+});

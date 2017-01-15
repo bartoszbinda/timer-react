4cec1c913ec98d7877f3b9b11aa89d8587ac7a90
Bartosz <batinex@gmail.com>
2017-01-15 17:51:26 +0100

    added validation of length of the number in countdownform

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -3,6 +3,10 @@ var CountdownForm = React.createClass({
   onSubmit: function (e) {
     e.preventDefault();
     var strSeconds = this.refs.seconds.value;
+    if(strSeconds.toString().length > 9) {
+      alert("The number you inserted is too big");
+      return ;
+    }
     if (strSeconds.match(/^[0-9]*$/)){
       this.refs.seconds.value = '';
       this.props.onSetCountdown(parseInt(strSeconds, 10));
